07d50d6853e0674b900821735fda050cbdae1fb2
dsurajev <d.surajev@gmail.com>
2020-07-12 16:01:36 +0300

    Updeitinta projekto versija.

--- projektas/scripts/scripts.js
+++ projektas/scripts/scripts.js
@@ -1,4 +1,10 @@
-
 function myFunction(x) {
-  x.classList.toggle("change");
+	x.classList.toggle('change');
 }
+
+const burgerMenu = document.getElementsByClassName('hamburger')[0]
+const navLinks = document.getElementsByClassName('nav-links')[0]
+
+burgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active')
+})
